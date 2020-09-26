c7ac72041ae61c5bd092f39abc7731aef30a468c
Alexander Czutro <github@czutro.ch>
2020-09-27 00:39:42 +0200

    code reformatting

--- src/libaczutro.cc
+++ src/libaczutro.cc
@@ -2,7 +2,7 @@
  *
  * useful-cpp - a collection of commonly-used functions for C++ programs
  *
- * Copyright 2005 - 2017 Alexander Czutro
+ * Copyright 2005 - 2020 Alexander Czutro
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,98 +19,135 @@
  *
  ******************************************************************* aczutro */
 
-#include <cstdarg>
-#include <cstring>
-
-#include <iostream>
-
 #include <libaczutro.h>
 
+#include <cstdarg>
+#include <cstring>
+#include <iostream>
 
-/*** printing information ****************************************************/
 
-void aczutro::error_and_exit(uint8_t status,
-                             bool recommend_help,
-                             const char *arg0,
-                             const char *message, ...
-                             ){
-    if(message && *message){
+using namespace aczutro;
+
+
+void error_and_exit(uint8_t status, bool recommend_help, const char* arg0, const char* message, ...)
+{
+    if(message && *message)
+    {
         std::cerr << arg0 << ": ";
+
         va_list ap;
-        const char *p = message;
         va_start(ap, message);
-        while(*p){
-            if(*p == '%'){
+
+        const char* p = message;
+        while(*p)
+        {
+            if(*p == '%')
+            {
                 p++;
-                switch(*p){
-                case 'i': std::cerr << va_arg(ap, long);	break;
-                case 'd': std::cerr << va_arg(ap, double);	break;
-                case 's': std::cerr << va_arg(ap, const char*);	break;
-                default:  std::cerr << "%";			break;
-                }//switch
-            }else{
+                switch(*p)
+                {
+                case 'i':
+                    std::cerr << va_arg(ap, long);
+                    break;
+                case 'd':
+                    std::cerr << va_arg(ap, double);
+                    break;
+                case 's':
+                    std::cerr << va_arg(ap, const char*);
+                    break;
+                default:
+                    std::cerr << "%";
+                    break;
+                }
+            }
+            else
+                {
                 std::cerr << *p;
-            }//else
+            }
             p++;
-        }//while
+        }
+
         va_end(ap);
+
         std::cerr << std::endl << std::flush;
-    }//if
-    if(recommend_help){
+    }
+
+    if(recommend_help)
+    {
         std::cerr << "Try '" << arg0 << " --help' for more information."
                   << std::endl << std::flush;
-    }//if
+    }
+
     exit(status);
-}//error_and_exit
+}
 
-/*****************************************************************/
 
-void aczutro::memory_dump(const char *label, const void *mem,
-                          uint64_t n, dump_mode mode){
+void memory_dump(const char* label, const void* mem, uint64_t n, dump_mode mode)
+{
     printf("%s:", label);
-    switch(mode){
-    case DUMP_BYTE:{
-        const uint8_t *p = reinterpret_cast<const uint8_t*>(mem);
-        for(uint i = 0; i < n; i++){
-            if((i % 16 == 0) && i){
-                printf("\n%*c", (int)(strlen(label) + 1), ' ');
-            }//if
-            if(i % 4 == 0){
-                printf("  ");
-            }//if
-            printf(" %02x", p[i]);
-        }//for
-        break;
-    }//DUMP_BYTE
-    case DUMP_HALF:{
-        const uint16_t *p = reinterpret_cast<const uint16_t*>(mem);
-        for(uint i = 0; i < n; i++){
-            if((i % 8 == 0) && i){
-                printf("\n%*c", (int)(strlen(label) + 1), ' ');
-            }//if
-            if(i % 2 == 0){
-                printf("  ");
-            }//if
-            printf(" %04x", p[i]);
-        }//for
-        break;
-    }//DUMP_HALF
-    case DUMP_WORD:{
-        const uint32_t *p = reinterpret_cast<const uint32_t*>(mem);
-        for(uint i = 0; i < n; i++){
-            if(i % 4 == 0){
-                if(i){
-                    printf("\n%*c", (int)(strlen(label) + 2), ' ');
-                }else{
-                    printf(" ");
-                }//else
-            }//if
-            printf("  %08x", p[i]);
-        }//for
-        break;
-    }//DUMP_WORD
-    }//switch
-    printf("\n");
-}//memory_dump
 
-/* aczutro ************************************************************* end */
+    switch(mode)
+    {
+    case DUMP_BYTE:
+    {
+        const auto p = reinterpret_cast<const uint8_t*>(mem);
+        for(uint i = 0; i < n; i++)
+        {
+            if((i % 16 == 0) && i)
+            {
+                printf("\n%*c", int(strlen(label) + 1), ' ');
+            }
+            if(i % 4 == 0)
+            {
+                printf("  ");
+            }
+            printf(" %02x", p[i]);
+        }
+        break;
+    }
+
+    case DUMP_HALF:
+    {
+        const auto p = reinterpret_cast<const uint16_t*>(mem);
+        for(uint i = 0; i < n; i++)
+        {
+            if((i % 8 == 0) && i)
+            {
+                printf("\n%*c", int(strlen(label) + 1), ' ');
+            }
+            if(i % 2 == 0)
+            {
+                printf("  ");
+            }
+            printf(" %04x", p[i]);
+        }
+        break;
+    }
+
+    case DUMP_WORD:
+    {
+        const auto p = reinterpret_cast<const uint32_t*>(mem);
+        for(uint i = 0; i < n; i++)
+        {
+            if(i % 4 == 0)
+            {
+                if(i)
+                {
+                    printf("\n%*c", int(strlen(label) + 2), ' ');
+                }
+                else
+                {
+                    printf(" ");
+                }
+            }
+            printf("  %08x", p[i]);
+        }
+        break;
+    }
+
+    }
+
+    printf("\n");
+}
+
+/* aczutro */
--- src/libaczutro.h
+++ src/libaczutro.h
@@ -2,7 +2,7 @@
  *
  * useful-cpp - a collection of commonly-used functions for C++ programs
  *
- * Copyright 2005 - 2017 Alexander Czutro
+ * Copyright 2005 - 2020 Alexander Czutro
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,15 +24,18 @@
 
 #include <cstdarg>
 #include <cstdint>
+#include <type_traits>
 
 namespace aczutro{
 
-#define ACZUTRO_VERSION "aczutro version 1.1"
+    /*!
+     * Returns a c string containing version information.
+     */
+    inline const char* version()
+    {
+        return "1.1";
+    }
 
-    /* Returns a c string containing version information. */
-    inline const char *version(){
-        return ACZUTRO_VERSION;
-    }//version
 
 
     /*** numerical tests *****************************************************/
@@ -41,101 +44,155 @@ namespace aczutro{
 #define is_odd(a)           ((a) % 2 == 1)
 #define is_between(a, b, c) (((b) <= (a)) && ((a) <= (c)))
 
-    /* If the first argument ('a') equals one of the arguments starting from
-     * the third position, returns the index of the first match (1 for the
-     * first argument after 'n', 2 for the second, etc.).  If there are no
-     * matches, returns 0.
-     * The second argument ('n') specifies the number of arguments that follow
-     * 'n'. */
-    template<typename T> uint16_t is_one_of(T a, uint8_t n, ...){
+
+    /*!
+     * If the first arguments equals one of the candidates, returns the index
+     * of the first match (starts from 1).
+     *
+     * If there are no matches, returns 0.
+     *
+     * \param a    number to compare
+     * \param n    number of candidates
+     * \param ...  the candidates
+     */
+    template<typename T> uint16_t is_one_of(T a, uint8_t n, ...)
+    {
         va_list args;
         va_start(args, n);
-        typedef typename std::conditional<
+
+        using U = typename std::conditional<
             (std::is_same<T, float>::value || std::is_same<T, double>::value),
             double,
             int
-            >::type U;
-        for(uint8_t i = 0; i < n; i++){
-            if(a == (T)va_arg(args, U)){
+            >::type;
+
+        for(uint8_t i = 0; i < n; i++)
+        {
+            if(a == (T)va_arg(args, U))
+            {
                 va_end(args);
                 return i + 1;
-            }//if
-        }//for
+            }
+        }
+
         va_end(args);
         return 0;
-    }//is_one_of
+    }
+
 
 
     /*** printing information ************************************************/
 
-    /* Prints an error message to STDERR and quits the main application.
-     * In addition, if 'recommend_help' is true, the message
-     * "Try 'arg0 --help' for more information." is also printed. */
-    void error_and_exit(uint8_t status,      // exit status
-                        bool recommend_help, // flag
-                        const char *arg0,    // the name of the application
-                        const char *message, // the error message
-                        ...                  // variable arguments to message
-                                             // (think printf)
-                        );
+    /*!
+     * Prints an error message to STDERR and quits the main application.
+     *
+     * If 'recommend_help' is true, the message
+     * "Try 'arg0 --help' for more information." is also printed.
+     *
+     * \param status  The exit status.
+     * \param arg0    The name of the application.
+     * \param message A formatted message, like in printf.
+     * \param ...     Variadic arguments referenced in the formatted message,
+     *                like in printf.
+     */
+    void error_and_exit(uint8_t status, bool recommend_help, const char* arg0,
+                        const char* message, ...);
 
-    enum dump_mode {DUMP_BYTE, DUMP_HALF, DUMP_WORD};
 
-    /* Prints 'n' bytes/halfwords/words of raw memory contents,
-     * starting at address 'mem'. */
-    void memory_dump(const char *label, const void *mem, uint64_t n,
-                     dump_mode mode=DUMP_BYTE);
+    /*!
+     * Operation mode of function memory_dump.
+     */
+    enum dump_mode
+    {
+        DUMP_BYTE,
+        DUMP_HALF,
+        DUMP_WORD
+    };
+
+
+    /*!
+     * Prints 'n' bytes/halfwords/words of raw memory contents,
+     * starting at address 'mem'.
+     *
+     * \param label A label to print at the beginning (e.g. variable name).
+     * \param mem   Address to start at.
+     * \param n     Number of elements to print.
+     * \param mode  Type of elements to print.
+     */
+    void memory_dump(const char* label, const void* mem, uint64_t n, dump_mode mode=DUMP_BYTE);
+
 
 
     /*** byte manipulation ***************************************************/
 
-    /* Concatenates two bytes. */
-    inline uint16_t make_halfword(uint8_t a, uint8_t b){
+    /*!
+     * Concatenates two bytes.
+     */
+    inline uint16_t make_halfword(uint8_t a, uint8_t b)
+    {
         return (a << 8) | b;
-    }//make_halfword
+    }
 
 
-    /* Writes into a1, a2 the two bytes contained in halfword. */
-    inline void unpack_halfword(uint16_t halfword, uint8_t *a1, uint8_t *a2){
+    /*!
+     * Writes into a1, a2 the two bytes contained in halfword.
+     */
+    inline void unpack_halfword(uint16_t halfword, uint8_t* a1, uint8_t* a2)
+    {
         *a1 = (halfword >> 8) & 0x00ff;
         *a2 = halfword & 0x00ff;
-        return;
-    }//unpack_halfword
+   }
 
 
-    /* Concatenates two halfwords, four bytes or one halfword and two bytes. */
-    inline uint32_t make_word(uint16_t a, uint16_t b){
+    /*!
+     * Concatenates two halfwords into a word.
+     */
+    inline uint32_t make_word(uint16_t a, uint16_t b)
+    {
         return (a << 16) | b;
-    }//make_word
+    }
 
-    inline uint32_t make_word(uint8_t a, uint8_t b, uint8_t c, uint8_t d){
+
+    /*!
+     * Concatenates four bytes into a word.
+     */
+    inline uint32_t make_word(uint8_t a, uint8_t b, uint8_t c, uint8_t d)
+    {
         return (a << 24) | (b << 16) | (c << 8) | d;
-    }//make_word
+    }
 
-    inline uint32_t make_word(uint16_t a, uint8_t b, uint8_t c){
+
+    /*!
+     * Concatenates a halfword and two bytes into a word.
+     */
+    inline uint32_t make_word(uint16_t a, uint8_t b, uint8_t c)
+    {
         return (a << 16) | (b << 8) | c;
-    }//make_word
+    }
 
 
-    /* Writes into a1, a2 the two halfwords contained in a word,
-     * or into a1, a2, a3, a4 the four bytes contained in a word. */
-    inline void unpack_word(uint32_t word, uint16_t *a1, uint16_t *a2){
+    /*!
+     * Writes into a1, a2 the two halfwords contained in a word.
+     */
+    inline void unpack_word(uint32_t word, uint16_t* a1, uint16_t* a2)
+    {
         *a1 = (word >> 16) & 0x0000ffff;
         *a2 = word & 0x0000ffff;
-        return;
-    }//unpack_word
+   }
 
-    inline void unpack_word(uint32_t word,
-                            uint8_t *a1, uint8_t *a2, uint8_t *a3, uint8_t *a4){
+    /*!
+     * Writes into a1, a2, a3 and a4 the four bytes contained in a word.
+     */
+    inline void unpack_word(uint32_t word, uint8_t* a1, uint8_t* a2, uint8_t* a3, uint8_t* a4)
+    {
         *a1 = (word >> 24) & 0x000000ff;
         *a2 = (word >> 16) & 0x000000ff;
         *a3 = (word >> 8) & 0x000000ff;
         *a4 = word & 0x000000ff;
-        return;
-    }//unpack_word
+   }
 
-}//aczutro
+} // namespace aczutro
 
 #endif
 
-/* aczutro ************************************************************* end */
+/* aczutro */
